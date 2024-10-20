1b2c1fbf9a52deaf547f8a848ce836aa262715e5
Valeriia Kanavchenko <165295440+valerie-kan@users.noreply.github.com>
2024-10-20 12:06:56 +0300

    Add search form

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,3 +1,10 @@
+import { RingLoader } from 'react-spinners';
+import css from './Loader.module.css';
+
 export const Loader = () => {
-  return <h2>Loader</h2>;
+  return (
+    <div className={css.backdrop}>
+      <RingLoader color="#20ca69" />
+    </div>
+  );
 };
--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,10 +1,50 @@
-import { Container, Heading, Section } from 'components';
+import {
+  Container,
+  CountryList,
+  Heading,
+  Loader,
+  SearchForm,
+  Section,
+} from 'components';
+import { useEffect, useState } from 'react';
+import { fetchByRegion } from 'service/countryApi';
 
 export const SearchCountry = () => {
+  const [countries, setCountries] = useState([]);
+  const [query, setQuery] = useState('');
+  const [error, setError] = useState(null);
+  const [loader, setLoader] = useState(false);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const getData = async () => {
+      setLoader(true);
+      try {
+        const infoFromServer = await fetchByRegion(query);
+
+        setCountries(infoFromServer);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoader(false);
+      }
+    };
+    getData();
+  }, [query]);
+
+  const onSubmit = query => {
+    setQuery(query);
+    setError(null);
+  };
+
   return (
     <Section>
       <Container>
-        <Heading title="SearchCountry" bottom />
+        {error && <Heading title={error} bottom />}
+        <SearchForm onSubmit={onSubmit} />
+        <CountryList data={countries} />
+        {loader && <Loader />}
       </Container>
     </Section>
   );
